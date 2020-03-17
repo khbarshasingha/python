53780dd5a2f14d47467070d9f8e415849327366f
Barsha <48600202+khbarshasingha@users.noreply.github.com>
2020-03-17 22:08:05 +0530

    Update cronclean.py

--- cronclean.py
+++ cronclean.py
@@ -6,12 +6,20 @@ import pretty_cron
 
 #cleanedlist = [line[:10] for line in clist if line[0]!='#']
 #print(cleanedlist)
+time=[]
+clist=[]
 with open('crontab_list_10_03_2020.txt') as reader:
     for line in reader.readlines():
-        if line[0]!='#' :
-            line=line.split('')
-        
-        print(line)
+        if line[0]!='#' and line[0]!='\n' and line[0]!=' ':
+            data=line.split(' ')
+            print(data[-1])
+            #time.append(line[:5])
+            #filepath=data[-3]
+            #clist.append(time)
+            #clist.append(filepath)
+        #print(line)
+print(data)
+
 
 #newlist=[pretty_cron.prettify_cron(time) for time in cleanedlist]  
 #print(newlist)  
